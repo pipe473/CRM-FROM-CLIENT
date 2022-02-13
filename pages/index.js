d48e8bfc51b7f9fe836c34bc6453355b7975155a
pipe473 <bedoyafelipe743@gmail.com>
2022-02-13 12:47:29 +0100

    Showing clients from seller

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { gql, useQuery } from '@apollo/client';
 const GET_CLIENTS_USER = gql`
   query getCustomerSeller {
     getCustomerSeller {
+      id
       nombre
       apellido
       empresa
@@ -22,13 +23,39 @@ console.log(data);
 console.log(loading);
 console.log(error);
 
-
+if (loading) return 'Cargando...'
 
 
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+
+
+      <table className="table-auto shadow-md mt-10 w-full w-lg">
+        <thead className="bg-gray-800">
+          <tr className="text-white">
+            <th className="w-1/5 py-2">Nombre</th>
+            <th className="w-1/5 py-2">Empresa</th>
+            <th className="w-1/5 py-2">Email</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {data.getCustomerSeller.map(cliente => (
+            <tr key={cliente.id}>
+              <td className="border px-4 py-2">
+                {cliente.nombre} {cliente.apellido}
+              </td>
+              <td className="border px-4 py-2">
+                {cliente.empresa}
+              </td>
+              <td className="border px-4 py-2">
+                {cliente.email}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       </Layout>
     </div>
   )
