cc6c8cec484385134240e9f0cd1e697c9490e8d0
pipe473 <bedoyafelipe743@gmail.com>
2022-02-14 20:48:12 +0100

    Creating new page for client

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import { gql, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const GET_CLIENTS_USER = gql`
   query getCustomerSeller {
@@ -27,17 +28,23 @@ console.log(error);
 
 if (loading) return 'Cargando...';
 
+// console.log(data.getCustomerSeller+'prueba carga');
+
  // Si no hay información
  if(!data.getCustomerSeller) {
   return router.push('/login');
 }
 
 
+
+
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
-
+        <Link href="/nuevoCliente">
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Cliente</a>
+        </Link>
 
       <table className="table-auto shadow-md mt-10 w-full w-lg">
         <thead className="bg-gray-800">
---
+++ pages/nuevoCliente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const nuevoCliente = () => {
+    return ( 
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Nuevo cliente</h1>
+        </Layout>
+     );
+}
+ 
+export default nuevoCliente;
