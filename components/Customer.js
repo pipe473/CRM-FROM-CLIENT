6aefdfb70bb5dc618ee97386cecc0bf43eb37517
pipe473 <bedoyafelipe743@gmail.com>
2022-02-26 01:09:29 +0100

    customer deleted from database

--- components/Customer.js
+++ components/Customer.js
@@ -1,32 +1,50 @@
 import React from "react";
 import Swal from "sweetalert2";
+import { gql, useMutation } from "@apollo/client";
+
+const DELETE_CLIENT = gql`
+  mutation deleteCustomer($id: ID!) {
+    deleteCustomer(id: $id)
+  }
+`;
 
 const Customer = ({ customer }) => {
+  // Mutation para eliminar cliente
+  const [deleteCustomer] = useMutation(DELETE_CLIENT);
+
   const { nombre, apellido, empresa, email, id } = customer;
 
   // Eliminar un cliente
-  const confirmarEliminarCliente = id =>{
-    // console.log('eliminando', id);    
+  const confirmarEliminarCliente = (id) => {
     Swal.fire({
-        title: '¿Deseas eliminar a este cliente',
-        text: "Esta acción no se puede deshacer!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, eliminar!',
-        cancelButtonText: 'No, cancelar'
-      }).then((result) => {
-        if (result.isConfirmed) {
-             console.log('eliminando', id);  
-          Swal.fire(
-            'Eliminado!',
-            'Your file has been deleted.',
-            'success'
-          )
+      title: "¿Deseas eliminar a este cliente?",
+      text: "Esta acción no se puede deshacer!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar!",
+      cancelButtonText: "No, cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        //  console.log('eliminando', id);
+        try {
+          // Eliminar por id
+          const { data } = await deleteCustomer({
+            variables: {
+              id
+            },
+          });
+          console.log(data);
+
+          // Mostrar alerta
+          Swal.fire("Eliminado!", data.deleteCustomer, "success");
+        } catch (error) {
+          console.log(error);
         }
-      })
-  }
+      }
+    });
+  };
 
   return (
     <tr>
@@ -37,22 +55,22 @@ const Customer = ({ customer }) => {
       <td className="border px-4 py-2">{email}</td>
       <td className="border px-4 py-2">
         <button
-            type="button"
-            className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
-            onClick={() => confirmarEliminarCliente(id) }
+          type="button"
+          className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+          onClick={() => confirmarEliminarCliente(id)}
         >
           Eliminar
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-4 w-4 ml-2"
+            className="h-4 w-4 ml-2"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
