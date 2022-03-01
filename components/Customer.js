27e0a13e17b5609fa9bcc54fd7523e89d761a91a
pipe473 <bedoyafelipe743@gmail.com>
2022-03-01 20:52:19 +0100

    Adding routing to edit client

--- components/Customer.js
+++ components/Customer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Swal from "sweetalert2";
 import { gql, useMutation } from "@apollo/client";
+import Router  from 'next/router';
 
 const DELETE_CLIENT = gql`
   mutation deleteCustomer($id: ID!) {
@@ -72,6 +73,13 @@ const Customer = ({ customer }) => {
     });
   };
 
+  const editarCliente = () => {
+    Router.push({
+        pathname: "/editarcliente/[id]",
+        query: { id }
+    })
+  }
+
   return (
     <tr>
       <td className="border px-4 py-2">
@@ -83,9 +91,32 @@ const Customer = ({ customer }) => {
         <button
           type="button"
           className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
-          onClick={() => confirmarEliminarCliente(id)}
+          onClick={() => confirmarEliminarCliente()}
         >
           Eliminar
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 ml-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+      </td>
+      <td className="border px-4 py-2">
+        <button
+          type="button"
+          className="flex justify-center items-center bg-green-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+          onClick={() => editarCliente()}
+        >
+          Editar
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-4 w-4 ml-2"
---
+++ pages/editarcliente/[id].js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const EditarCliente = () => {
+
+    // Obtener el ID actual
+    const router = useRouter();
+
+    const { query: { id } } = router;
+
+    console.log(id);
+    
+
+    return ( 
+        <h1>Desde editar</h1>
+     );
+}
+ 
+export default EditarCliente;
