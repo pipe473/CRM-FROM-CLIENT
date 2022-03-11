e827b091a918dc5ced1ecc2ce1b22fbc803f395b
pipe473 <bedoyafelipe743@gmail.com>
2022-03-11 20:42:13 +0100

    Adding formik to new product form

--- pages/nuevoproducto.js
+++ pages/nuevoproducto.js
@@ -1,7 +1,33 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const NuevoProducto = () => {
+
+// Formulario para nuevos productos
+const formik = useFormik({
+    initialValues: {
+        nombre: '',
+        stock: '',
+        precio: ''
+    },
+    validationSchema: Yup.object({
+        nombre: Yup.string()
+                .required('El nombre del producto es obligatorio'),
+        stock: Yup.number()
+                  .required('Añade la cantidad disponible')
+                  .positive('No se aceptan números negativos')
+                  .integer('El stock debe ser con números enteros'),
+        precio: Yup.number()
+                    .required('El precio es obligatorio')
+                    .positive('No se aceptan números negativos')
+    }),
+    onSubmit: async val => {
+
+    }
+})
+
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">
@@ -12,7 +38,7 @@ const NuevoProducto = () => {
         <div className="w-full max-w-lg">
           <form
             className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
-            // onSubmit={formik.handleSubmit}
+            onSubmit={formik.handleSubmit}
           >
             <div className="mb-4">
               <label
@@ -26,11 +52,17 @@ const NuevoProducto = () => {
                 id="nombre"
                 type="text"
                 placeholder="Nombre Producto"
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.nombre}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.nombre}
               />
             </div>
+            {formik.touched.nombre && formik.errors.nombre ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.nombre}</p>
+              </div>
+            ) : null}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -43,11 +75,17 @@ const NuevoProducto = () => {
                 id="stock"
                 type="number"
                 placeholder="Cantidad Disponible"
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.nombre}
+                 onChange={formik.handleChange}
+                 onBlur={formik.handleBlur}
+                 value={formik.values.stock}
               />
             </div>
+            {formik.touched.stock && formik.errors.stock ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.stock}</p>
+              </div>
+            ) : null}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -60,11 +98,17 @@ const NuevoProducto = () => {
                 id="precio"
                 type="number"
                 placeholder="Precio Producto"
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.nombre}
+                 onChange={formik.handleChange}
+                 onBlur={formik.handleBlur}
+                 value={formik.values.precio}
               />
             </div>
+            {formik.touched.precio && formik.errors.precio ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.precio}</p>
+              </div>
+            ) : null}
               <input
                 type="submit"
                 className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900"
