d559957941f120de6b3721bae336ebf64f5f2a42
pipe473 <bedoyafelipe743@gmail.com>
2022-04-10 12:11:07 +0200

    Finishing useReducer configuration

--- context/pedidos/PedidoState.js
+++ context/pedidos/PedidoState.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, Children } from 'react';
 import PedidoContext from './PedidoContext';
 import PedidoReducer from './PedidoReducer';
 
@@ -7,3 +7,30 @@ import {
     SELECT_PRODUCT,
     PRODUCTS_QUANTIY
 } from '../../types'
+
+const PedidoState = ({children}) => {
+
+    // state de Pedidos
+    const initialState = {
+        client: [],
+        products: [],
+        total: 0
+    }
+
+    const [ state, dispatch ] = useReducer(PedidoReducer, initialState);
+
+    const holaMundoEnUserReducer = () => {
+        console.log('Hello world');        
+    }
+
+    return ( 
+        <PedidoContext.Provider        
+            value={{
+                holaMundoEnUserReducer
+            }}
+        > {children}
+        </PedidoContext.Provider>
+     );
+}
+ 
+export default PedidoState;
