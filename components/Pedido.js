1f07994780b2e46ee866c3d7551c3006b8902eed
pipe473 <bedoyafelipe743@gmail.com>
2022-04-22 17:37:51 +0200

    Creating component to orders using query

---
+++ components/Pedido.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pedido = ({ order }) => {
+    return ( 
+        <h1>{ order.total }</h1>
+     );
+}
+ 
+export default Pedido;
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,16 +1,54 @@
 import Layout from '../components/Layout';
+import Pedido from '../components/Pedido';
 import Link from 'next/link';
+import { gql, useQuery } from '@apollo/client';
 
-const Pedidos = () => (
-  <div>
-    <Layout>
-        <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
 
-        <Link href="/nuevopedido">
-          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Pedido</a>
-        </Link>
-    </Layout>
-  </div>
-)
+const OBTENER_PEDIDOS = gql`
+  query getOrdersBySeller {
+    getOrdersBySeller {
+      id
+      order {
+        id
+        quantity
+      }
+      cliente
+      vendedor
+      total
+      estado
+    }
+  }
+`;
+
+const Pedidos = () => {
+
+const { data, loading, error } = useQuery( OBTENER_PEDIDOS );
+
+if(loading) return 'Cargando...';
+
+const { getOrdersBySeller } = data;
+
+  return (
+    <div>
+      <Layout>
+          <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+  
+          <Link href="/nuevopedido">
+            <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Pedido</a>
+          </Link>
+          { getOrdersBySeller.length === 0 ? (
+            <p className="bg-yellow-300 mt-5 text-center text-2xl py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">No hay pedidos aún</p>
+          ) : (
+            getOrdersBySeller.map( order => (
+              <Pedido 
+                key={order.id}
+                order={order}
+              />
+            ))
+          ) }
+      </Layout>
+    </div>
+  )
+}
 
 export default Pedidos;
