3741c34c483e653c4df2efff162d487e300a970d
pipe473 <bedoyafelipe743@gmail.com>
2022-05-02 13:46:46 +0200

    Showing best sellers graphics

--- pages/mejoresvendedores.js
+++ pages/mejoresvendedores.js
@@ -3,62 +3,56 @@ import Layout from '../components/Layout';
 // import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+import { gql, useQuery } from '@apollo/client';
+
+const BEST_SELLERS = gql`
+    query mejoresVendedores {
+        mejoresVendedores {
+        vendedor {
+            nombre
+            email
+        }
+        total
+        }
+    }
+`;
 
 const MejoresVendedores = () => {
+
+    const { data, loading, error } = useQuery( BEST_SELLERS );
+
+    console.log(loading);
+    console.log(error);  
+    
+    if(loading) return 'cargando...'
+    console.log(data.mejoresVendedores);
+
+    const { mejoresVendedores } = data;
+
+    const graphSeller = [];
+
+    mejoresVendedores.map((vendedor, index) => {
+        graphSeller[index] = {
+            ...vendedor.vendedor[0],
+            total: vendedor.total
+        };
+    })
+
+    console.log(graphSeller);
+    
+    
+
     return ( 
         <Layout>
              <h1 className="text-2xl text-gray-800 font-light">Mejores Vendedores</h1>
              <ResponsiveContainer width="50%" height="50%">
-                <BarChart width={400} height={600} data={data} margin={{top: 5, right:30, left: 20, bottom: 5}}>
+                <BarChart width={400} height={600} data={graphSeller} margin={{top: 5, right:30, left: 20, bottom: 5}}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="nombre" />
+                <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="pv" fill="#3182CE" />
+                <Bar dataKey="total" fill="#3182CE" />
                 {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
                 </BarChart>
             </ResponsiveContainer>
