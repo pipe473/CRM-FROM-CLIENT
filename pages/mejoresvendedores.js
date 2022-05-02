2fd38d689a4e149c3a9b375010a8ea52a7d18572
pipe473 <bedoyafelipe743@gmail.com>
2022-05-02 13:15:28 +0200

    Creating the best sellers and recharts

---
+++ pages/mejoresvendedores.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const MejoresVendedores = () => {
+    return ( 
+        <Layout>
+             <h1 className="text-2xl text-gray-800 font-light">Mejores Vendedores</h1>
+        </Layout>
+     );
+}
+ 
+export default MejoresVendedores;
