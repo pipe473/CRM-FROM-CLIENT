e821461ed84cd7544c504b3895c84a3a11ae0d29
pipe473 <bedoyafelipe743@gmail.com>
2022-05-09 21:43:28 +0200

    Adding graphics to best customers

---
+++ pages/mejoresclientes.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import Layout from '../components/Layout';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import { gql, useQuery } from '@apollo/client';
+
+const BEST_CUSTOMERS = gql`
+    query mejoresClientes {
+        mejoresClientes {
+        cliente {
+            nombre
+            empresa
+        }
+        total
+        }
+    }
+`;
+
+const MejoresClientes = () => {
+
+    const { data, loading, error, startPolling, stopPolling } = useQuery( BEST_CUSTOMERS );
+
+    useEffect(() => {
+        startPolling(1000);
+        return () => {
+            stopPolling();
+        }
+    }, [ startPolling, stopPolling ])
+
+    // console.log(loading);
+    // console.log(error);  
+    
+    if(loading) return 'cargando...';
+    console.log(data);
+
+    const { mejoresClientes } = data;
+
+    const graphCustomer = [];
+
+    mejoresClientes.map((cliente, index) => {
+        graphCustomer[index] = {
+            ...cliente.cliente[0],
+            total: cliente.total
+        };
+    });
+    
+    
+
+    return ( 
+        <Layout>
+             <h1 className="text-2xl text-gray-800 font-light">Mejores Clientes</h1>
+             <ResponsiveContainer width="50%" height="50%">
+                <BarChart width={400} height={600} data={graphCustomer} margin={{top: 5, right:30, left: 20, bottom: 5}}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="nombre" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" fill="#3182CE" />
+                {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+                </BarChart>
+            </ResponsiveContainer>
+        </Layout>
+     );
+}
+ 
+export default MejoresClientes;
